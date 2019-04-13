e608abfe64a99f3f75a5d214e30379a16f4ba4a7
bilgeejargalsaihan <bilgee.py@gmail.com>
2019-04-12 22:38:45 -0500

    user login using passport

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://admin:admin123@ds151558.mlab.com:51558/developercon'
+  mongoURI: 'mongodb://admin:admin123@ds151558.mlab.com:51558/developercon',
+  secret: 'secret'
 }
